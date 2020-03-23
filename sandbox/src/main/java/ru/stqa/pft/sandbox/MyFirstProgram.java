dabf613a4b83051c3c9e2c145063d05ebe572af4
IraKotova <62184548+IraKotova@users.noreply.github.com>
2020-03-23 22:08:27 +0300

    Добавлены новые классы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,25 +6,18 @@ public class MyFirstProgram {
 		hello("user");
 		hello("Ira");
 
-		double l = 5;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-		double a = 4;
-		double b = 6;
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+		Rectangle r = new Rectangle(4,6);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+		Point p = new Point (1,3,7,10);
+		System.out.println("Расстояние между точками р1 (" + p.x1 + ";" + p.y1 + ") и p2 (" + p.x2 + ";" + p.y2 + ") = " + p.area());
 	}
 
 	public static void hello(String somebody){
 		System.out.println("Hello," + somebody +  "!");
 	}
-
-	public static double area (double len){
-		return len*len;
-	}
-
-	public static double area (double a, double b){
-		return a*b;
-	}
-
 
 }
